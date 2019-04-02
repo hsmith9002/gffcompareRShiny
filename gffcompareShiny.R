3deecaafeb954c1a0f94d56c22081bb6bbdf44a6
Harry Smith <harry.smith@ucdenver.edu>
2019-04-02 12:12:06 -0500

    added plotOutput()

--- gffcompareShiny.R
+++ gffcompareShiny.R
@@ -24,7 +24,8 @@ ui <- fluidPage(
               label = "Select a dataset (gtf)", 
               choices =  list(`HRDP` = c("Brain", "Liver"),
                               `HS` = c("BR1", "BR2", "BR3", "BR4", "BR5"),
-                              `Legacy` = c("Tissue1", "Tissue2", "Tissue3")))
+                              `Legacy` = c("Tissue1", "Tissue2", "Tissue3"))),
+  plotOutput(outputId = "bar")
 )
 
 ## Sect 2: SERVER
