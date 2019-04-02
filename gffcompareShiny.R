51e64d74b4563552c93e3efa22597605ca90f9f4
Harry Smith <harry.smith@ucdenver.edu>
2019-04-02 11:01:53 -0500

    Created R Project for gffcompareShiny

---
+++ gffcompareShiny.R
@@ -0,0 +1,16 @@
+#########################################
+# Title: gffcompareShiny
+# Author: Harry Smith
+# Description: This is a program for an 
+# RShiny app that takes as input a gtf
+# from gffcompare and summuarizes/
+# visualizes the class codes using ggplot2
+#########################################
+
+library(shiny)
+
+## Sect 1: UI
+
+## Sect 2: SERVER
+
+## Sect 3: KNIT
