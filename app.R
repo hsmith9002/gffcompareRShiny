bc90ce190692aed403d2ea71d9c96eb8573f3164
Harry Smith <harry.smith@ucdenver.edu>
2019-04-03 10:41:34 -0500

    trying ggsave method for plot download

--- app.R
+++ app.R
@@ -27,7 +27,6 @@ ui <- fluidPage(
            table do not include 'intergenic'unknown' comparisons
            as defined by gffcompare. All other codes 
            are used exactly as defined on gffcompare manual."),
-  radioButtons("var1", label = "Download plot file type", choices = list("png", "pdf")),
   ## Input functions
   fileInput(inputId = "file1", 
             label = "Upload gtf",
@@ -41,121 +40,124 @@ ui <- fluidPage(
 ## Sect 2: SERVER
 server <- function(input, output, session) {
   ## generate output bar chart 
+  
+  ## prepare plot objects
+  data <- reactiveValues()
+  inFile <- input$file1
+  if (is.null(inFile))
+    return(NULL)
+  path <- inFile$datapath
+  
+  ############################
+  #import gtf
+  ############################
+  
+  progress <- Progress$new(session, min=0, max=10)
+  on.exit(progress$close())
+  
+  progress$set(message = 'Importing and parsing gtf file using RTracklayer',
+               detail = 'This may take a few minutes...')   
+  
+  df <- import(path)
+  df <- as.data.frame(df)
+  
+  ############################
+  #Summarize class codes
+  ############################
+  
+  ## Filter exon rows
+  irfilt <- df %>% 
+    filter(type == "transcript")
+  ## Generate data sets for intergenic and nonintergenic transcripts
+  interg <- irfilt %>% filter(class_code == "u")
+  noninterg <- irfilt %>% filter(class_code != "u")
+  if(sum(dim(interg)[1], dim(noninterg)[1]) != dim(irfilt)[1]) stop("Dimension of filtered data sets do not equal original")
+  ## Summarize class codes for non-intergenic transcripts and return
+  noninterg$class_code <- factor(noninterg$class_code, levels = c("=", "c", "k", "m", "n", "j", "e", "o", "s", "x", "i", "y", "p", "r", "u", "z"), 
+                                 labels = c("Exact match", "Contained in reference", "Containment of reference", 
+                                            "Full intron chain match", "partial intron chain match", "At least 1 exon junction match", 
+                                            "single exon partially covering intron", "other same strand overlap", "intron match on opposite strand",
+                                            "exon overlap on opposite strand", "fully contained within reference intron", "contains a reference within introns",
+                                            "no overlap", "repeat", "intergenic", "z"))
+  out <- as.data.frame(table(noninterg$class_code))
+  levs <- factor(c("=", "c", "k", "m", "n", "j", "e", "o", "s", "x", "i", "y", "p", "r", "u", "z"),
+                 levels = c("=", "c", "k", "m", "n", "j", "e", "o", "s", "x", "i", "y", "p", "r", "u", "z"), 
+                 labels = c("Exact match", "Contained in reference", "Containment of reference", 
+                            "Full intron chain match", "partial intron chain match", "At least 1 exon junction match", 
+                            "single exon partially covering intron", "other same strand overlap", "intron match on opposite strand",
+                            "exon overlap on opposite strand", "fully contained within reference intron", "contains a reference within introns",
+                            "no overlap", "repeat", "intergenic", "z"))
+  labs <- c("Exact match", "Contained in reference", "Containment of reference", 
+            "Full intron chain match", "partial intron chain match", "At least 1 exon junction match", 
+            "single exon partially covering intron", "other same strand overlap", "intron match on opposite strand",
+            "exon overlap on opposite strand", "fully contained within reference intron", "contains a reference within introns",
+            "no overlap", "repeat", "intergenic", "z")
+  out.levs <- out$Var1[which(out$Var1 %in% levs)]
+  out.labs <- out$Var1[which(out$Var1 %in% labs)]
+  out$Var1 <- factor(out$Var1, levels = out.levs, labels = out.labs)
+  out$prop <- (out$Freq/dim(irfilt)[1])
+  out$Perc <- percent((out$Freq/dim(irfilt)[1]))
+  ## Check to make sure the number of class codes in summary sums to the total number of non-intergenic transcripts
+  if(sum(out$Freq) != dim(noninterg)[1]) stop("Total class codes do not sum to total non-intergenic txts")
+  out <- as.data.frame(out)
+  output$table <- renderTable(out[-length(out$Var1), ])
+  ############################
+  #Prepare for plotting
+  ############################
+  
+  bnccc_man <- out %>% 
+    filter(Var1 %in% c("Exact match", "Contained in reference", 
+                       "Containment of reference", "At least 1 exon junction match")) %>%
+    mutate(Var1 = factor(as.character(Var1), 
+                         levels = levs, 
+                         labels = labs)) 
+  ccvec <- setdiff(c("Exact match", "Contained in reference", 
+                     "Containment of reference", "At least 1 exon junction match"), bnccc_man$Var1)
+  bnccc_tmp <- data.frame(Var1 = ccvec, 
+                          Freq = as.numeric(rep(0, length(ccvec))), 
+                          prop = as.numeric(rep(0, length(ccvec))),
+                          Perc = percent(as.numeric(rep(0, length(ccvec)))))
+  
+  bnccc_other <- c("z", sum(out$Freq[which(out$Var1 %!in% c("Exact match", "Contained in reference", 
+                                                            "Containment of reference", "At least 1 exon junction match"))]),
+                   sum(out$prop[which(out$Var1 %!in% c("Exact match", "Contained in reference", 
+                                                       "Containment of reference", "At least 1 exon junction match"))]),
+                   percent(sum(out$prop[which(out$Var1 %!in% c("Exact match", "Contained in reference", 
+                                                               "Containment of reference", "At least 1 exon junction match"))])))
+  bnccc_man <- rbind(bnccc_man, bnccc_tmp, bnccc_other)
+  bnccc_man <- bnccc_man %>%
+    mutate(prop = as.numeric(prop))
+  bnccc_test <- bnccc_man[order(levs[which(levs %in% bnccc_man$Var1)]), ]
+  
+  ############################
+  #Generate plot
+  ############################
+  
+  data$plot <- ggplot(bnccc_man, aes(x=Var1, y=prop, fill=Var1)) +
+    geom_bar(stat="identity") +
+    scale_y_continuous(labels=percent, 
+                       limits=c(0,0.70)) + 
+    scale_x_discrete(labels=c("Exact match", 
+                              "Contained in \n reference", 
+                              "Containment of \n reference", 
+                              "At least 1 \n exon junction \n match",
+                              "Ambiguous overlap")) +
+    scale_fill_manual('leged', values = brewer.pal(n = 5, name = "Set2")) +
+    theme(legend.position='none',
+          plot.title = element_text(hjust=0, size = 20)) +
+    geom_text(data=bnccc_man, 
+              aes(label=Perc[order(bnccc_man$Var1)], 
+                  y=prop[order(bnccc_man$Var1)]+0.015,
+                  x = (seq(1,5,1))+0.085), 
+              size=5.5) +
+    xlab("Class Code") + 
+    ylab(paste0("Number of Transcripts non-intergenic: ",
+                prettyNum(sum(as.numeric(bnccc_man$Freq)), 
+                          big.mark = ",", 
+                          big.interval = 3))) 
+  
   output$bar <- renderPlot({
-
-    inFile <- input$file1
-    if (is.null(inFile))
-      return(NULL)
-    path <- inFile$datapath
-    
-      ############################
-      #import gtf
-      ############################
-    
-    progress <- Progress$new(session, min=0, max=10)
-    on.exit(progress$close())
-    
-    progress$set(message = 'Importing and parsing gtf file using RTracklayer',
-                 detail = 'This may take a few minutes...')   
-    
-      df <- import(path)
-      df <- as.data.frame(df)
-      
-      ############################
-      #Summarize class codes
-      ############################
-      
-      ## Filter exon rows
-      irfilt <- df %>% 
-        filter(type == "transcript")
-      ## Generate data sets for intergenic and nonintergenic transcripts
-      interg <- irfilt %>% filter(class_code == "u")
-      noninterg <- irfilt %>% filter(class_code != "u")
-      if(sum(dim(interg)[1], dim(noninterg)[1]) != dim(irfilt)[1]) stop("Dimension of filtered data sets do not equal original")
-      ## Summarize class codes for non-intergenic transcripts and return
-      noninterg$class_code <- factor(noninterg$class_code, levels = c("=", "c", "k", "m", "n", "j", "e", "o", "s", "x", "i", "y", "p", "r", "u", "z"), 
-                                     labels = c("Exact match", "Contained in reference", "Containment of reference", 
-                                                "Full intron chain match", "partial intron chain match", "At least 1 exon junction match", 
-                                                "single exon partially covering intron", "other same strand overlap", "intron match on opposite strand",
-                                                "exon overlap on opposite strand", "fully contained within reference intron", "contains a reference within introns",
-                                                "no overlap", "repeat", "intergenic", "z"))
-      out <- as.data.frame(table(noninterg$class_code))
-      levs <- factor(c("=", "c", "k", "m", "n", "j", "e", "o", "s", "x", "i", "y", "p", "r", "u", "z"),
-                     levels = c("=", "c", "k", "m", "n", "j", "e", "o", "s", "x", "i", "y", "p", "r", "u", "z"), 
-                     labels = c("Exact match", "Contained in reference", "Containment of reference", 
-                                "Full intron chain match", "partial intron chain match", "At least 1 exon junction match", 
-                                "single exon partially covering intron", "other same strand overlap", "intron match on opposite strand",
-                                "exon overlap on opposite strand", "fully contained within reference intron", "contains a reference within introns",
-                                "no overlap", "repeat", "intergenic", "z"))
-      labs <- c("Exact match", "Contained in reference", "Containment of reference", 
-                "Full intron chain match", "partial intron chain match", "At least 1 exon junction match", 
-                "single exon partially covering intron", "other same strand overlap", "intron match on opposite strand",
-                "exon overlap on opposite strand", "fully contained within reference intron", "contains a reference within introns",
-                "no overlap", "repeat", "intergenic", "z")
-      out.levs <- out$Var1[which(out$Var1 %in% levs)]
-      out.labs <- out$Var1[which(out$Var1 %in% labs)]
-      out$Var1 <- factor(out$Var1, levels = out.levs, labels = out.labs)
-      out$prop <- (out$Freq/dim(irfilt)[1])
-      out$Perc <- percent((out$Freq/dim(irfilt)[1]))
-      ## Check to make sure the number of class codes in summary sums to the total number of non-intergenic transcripts
-      if(sum(out$Freq) != dim(noninterg)[1]) stop("Total class codes do not sum to total non-intergenic txts")
-      out <- as.data.frame(out)
-      output$table <- renderTable(out[-length(out$Var1), ])
-      ############################
-      #Prepare for plotting
-      ############################
-      
-      bnccc_man <- out %>% 
-        filter(Var1 %in% c("Exact match", "Contained in reference", 
-                           "Containment of reference", "At least 1 exon junction match")) %>%
-        mutate(Var1 = factor(as.character(Var1), 
-                             levels = levs, 
-                             labels = labs)) 
-      ccvec <- setdiff(c("Exact match", "Contained in reference", 
-                         "Containment of reference", "At least 1 exon junction match"), bnccc_man$Var1)
-      bnccc_tmp <- data.frame(Var1 = ccvec, 
-                              Freq = as.numeric(rep(0, length(ccvec))), 
-                              prop = as.numeric(rep(0, length(ccvec))),
-                              Perc = percent(as.numeric(rep(0, length(ccvec)))))
-      
-      bnccc_other <- c("z", sum(out$Freq[which(out$Var1 %!in% c("Exact match", "Contained in reference", 
-                                                                "Containment of reference", "At least 1 exon junction match"))]),
-                       sum(out$prop[which(out$Var1 %!in% c("Exact match", "Contained in reference", 
-                                                           "Containment of reference", "At least 1 exon junction match"))]),
-                       percent(sum(out$prop[which(out$Var1 %!in% c("Exact match", "Contained in reference", 
-                                                                   "Containment of reference", "At least 1 exon junction match"))])))
-      bnccc_man <- rbind(bnccc_man, bnccc_tmp, bnccc_other)
-      bnccc_man <- bnccc_man %>%
-        mutate(prop = as.numeric(prop))
-      bnccc_test <- bnccc_man[order(levs[which(levs %in% bnccc_man$Var1)]), ]
-      
-      ############################
-      #Generate plot
-      ############################
-      
-      pbnlx2.pct.man <- ggplot(bnccc_man, aes(x=Var1, y=prop, fill=Var1)) +
-        geom_bar(stat="identity") +
-        scale_y_continuous(labels=percent, 
-                           limits=c(0,0.70)) + 
-        scale_x_discrete(labels=c("Exact match", 
-                                  "Contained in \n reference", 
-                                  "Containment of \n reference", 
-                                  "At least 1 \n exon junction \n match",
-                                  "Ambiguous overlap")) +
-        scale_fill_manual('leged', values = brewer.pal(n = 5, name = "Set2")) +
-        theme(legend.position='none',
-              plot.title = element_text(hjust=0, size = 20)) +
-        geom_text(data=bnccc_man, 
-                  aes(label=Perc[order(bnccc_man$Var1)], 
-                      y=prop[order(bnccc_man$Var1)]+0.015,
-                      x = (seq(1,5,1))+0.085), 
-                  size=5.5) +
-        xlab("Class Code") + 
-        ylab(paste0("Number of Transcripts non-intergenic: ",
-                    prettyNum(sum(as.numeric(bnccc_man$Freq)), 
-                              big.mark = ",", 
-                              big.interval = 3))) 
-    pbnlx2.pct.man
+    data$plot
   }
   )
   ## Download plot
@@ -164,12 +166,7 @@ server <- function(input, output, session) {
       paste("classCode", input$var1, sep = ".")
     },
     content = function(file) {
-      if(input$var1 == "png")
-        png(file)
-      else
-        pdf(file)
-      print(pbnlx2.pct.man)
-      dev.off()
+     ggsave(file, data$plot)
     }
   )
 }
